35ade234e1622799151e13433005e080f925ea85
M45Hasan <mmhasan045@gmail.com>
2022-09-11 09:58:43 +0600

    navBanner__done
    first part completed

---
+++ js/tailwind.config.js
@@ -0,0 +1,28 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        red: "#F40404",
+        yellow: "rgb(250 204 21)",
+        bag: "rgba(238, 237, 222, 1)",
+        bag1: "#FFFFFF",
+        tx1: "#203239",
+        bder:"rgba(255, 255, 255, 1)",
+        but1:"rgba(32, 50, 57, 1)",
+        pls:"rgba(255, 117, 39, 1)",
+        bo:"rgba(238, 237, 222, 1)",
+        sr:"rgba(214, 213, 199, 1)",
+      },
+      maxWidth: {
+        conatiner: "1164px",
+      },
+      fontFamily: {
+        pop: "Poppins",
+      },
+      backgroundImage: {
+        ban: "url('hom1.png')",
+      },
+    },
+  },
+};
