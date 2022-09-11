7e01bf27cc0c5405ff90ed05cc45959a2ffbf632
M45Hasan <mmhasan045@gmail.com>
2022-09-11 17:53:14 +0600

    footer__completed

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -3,13 +3,22 @@ tailwind.config = {
     extend: {
       colors: {
         clifford: "#da373d",
+        white:"rgba(238, 237, 222, 1)",
         red: "#F40404",
         yellow: "rgb(250 204 21)",
         bag: "rgba(238, 237, 222, 1)",
         bag1: "#FFFFFF",
+        bag4:"rgba(255, 255, 255, 1)",
         tx1: "#203239",
         bder: "rgba(255, 255, 255, 1)",
         but1: "rgba(32, 50, 57, 1)",
+        bbo: "rgba(214, 213, 199, 1)",
+        angb: "rgba(214, 213, 199, 0.3)",
+        but2: "rgba(255, 117, 39, 0.2)",
+        org: "rgba(255, 117, 39, 1)",
+        
+
+
         pls: "rgba(255, 117, 39, 1)",
         bo: "rgba(238, 237, 222, 1)",
         sr: "rgba(214, 213, 199, 1)",
@@ -23,6 +32,9 @@ tailwind.config = {
       backgroundImage: {
         ban: "url('hom1.png')",
         fv2: "url('fv2.png')",
+        res1: "url('res1.png')",
+        res2: "url('res2.png')",
+        res3: "url('res3.png')",
       },
     },
   },
